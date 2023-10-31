8c6f12e3e6223230e361fc0bc5eedb6cd8bca628
Henry Meager <henry.meager@gmail.com>
2023-10-31 14:57:10 +0000

    removing bookswap folder and tracking nav folder

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,23 @@
+import { Link, NavLink } from "react-router-dom";
+import "./Nav.css";
+function Nav() {
+  return (
+    <nav className="navigation">
+      <div>
+        <h1>Book Swap</h1>
+      </div>
+      <div className="links">
+        <NavLink activeClass="active" to="availableBooks">
+          Available books
+        </NavLink>
+        <NavLink activeClass="active" to="claimedBooks">
+          Claimed books
+        </NavLink>
+        <NavLink activeClass="active" to="addBook">
+          Add book
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
