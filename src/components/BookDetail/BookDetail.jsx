43f25650949dd79a29178040d75e4c3e08bc0f03
Timothy Williamson <atimiw@gmail.com>
2023-10-31 16:34:03 +0000

    Added error handling for invalid book references (placeholder text for time being.

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -14,6 +14,8 @@ function BookDetail() {
     const [bookGenre, setBookGenre] = useState('');
     const [bookBlurb, setBookBlurb] = useState('');
 
+    const[error, setError] = useState(false)
+
     //Fetch the individual book data
     useEffect(function() {
 
@@ -26,32 +28,56 @@ function BookDetail() {
         method: "GET",
     }).then(res => res.json())
         .then(bookData => {
-            setBookImage(bookData.data.image);
-            setBookTitle(bookData.data.title);
-            setBookAuthor(bookData.data.author);
-            setBookYear(bookData.data.year);
-            setBookPageCount(bookData.data.page_count);
-            setBookGenre(bookData.data.genre.name);
-            setBookBlurb(bookData.data.blurb);
+
+            // If bookData has a key of data, we know the request worked
+            if('data' in bookData)  {
+                setBookImage(bookData.data.image);
+                setBookTitle(bookData.data.title);
+                setBookAuthor(bookData.data.author);
+                setBookYear(bookData.data.year);
+                setBookPageCount(bookData.data.page_count);
+                setBookGenre(bookData.data.genre.name);
+                setBookBlurb(bookData.data.blurb);
+            } else {
+                // Display an error message
+                setError(true)
+            }
+
+
         })
 
     }, [id])
 
     return (
-        <div className="detail-container">
-            <div>
-                <img className="detail-container-image" src={bookImage} alt={bookTitle} />
-            </div>
-            <div className="detail-container-text">
-                <h1>{bookTitle}</h1>
-                <p>{bookAuthor}</p>
-                <p>{bookYear}</p>
-                <p>{bookPageCount}</p>
-                <p>{bookGenre}</p>
-                <p>{bookBlurb}</p>
-            </div>
+        <div>
+            {error ? 
+                <div> 
+                    <p>
+                        "Error: Book not found!"    
+                    </p>
+                </div>
+
+                :
+                <div className="detail-container">
+                    <div>
+                        <img className="detail-container-image" src={bookImage} alt={bookTitle} />
+                    </div>
+                    <div className="detail-container-text">
+                        <h1>{bookTitle}</h1>
+                        <p>{bookAuthor}</p>
+                        <p>{bookYear}</p>
+                        <p>{bookPageCount}</p>
+                        <p>{bookGenre}</p>
+                        <p>{bookBlurb}</p>
+                    </div>
+                </div>                
+            } 
+
+
         </div>
+
     )
+
 
 }
 
