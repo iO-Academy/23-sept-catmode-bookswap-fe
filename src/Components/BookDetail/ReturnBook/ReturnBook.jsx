3ecb0709b0d7a8667a6fa5e9b23cfe71c7144375
Kellie Marsh <miss.kellie.marsh@gmail.com>
2023-11-02 16:17:49 +0000

    added the return book component

---
+++ src/Components/BookDetail/ReturnBook/ReturnBook.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import "./ReturnBook.css"
+
+function ReturnBook({ claimedByName, bookID, setClaimedByName}) {
+    
+    const [email, setEmail] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const [name, setname] = useState('')
+
+    function emailChange(event) {
+        setEmail(event.target.value)
+    }
+
+    function submitBookReturn(event) {
+        event.preventDefault()
+    
+    fetch(`https://book-swap-api.dev.io-academy.uk/api/books/return/${bookID}`, {
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            method: "PUT",
+            body: JSON.stringify({
+                'email': email
+            })
+        }).then((res) =>
+            {
+                if (res.ok) { 
+                    return res.json()
+                    .then(data => {
+                        setClaimedByName (null)
+                        setErrorMessage("")
+                    }
+                    )
+                   }
+                else {
+                    res.json()
+                    .then(data => {
+                        setErrorMessage(data.message)           
+                    })
+                }
+            }
+        )   
+    }
+
+       function ReturnForm() {
+            return (
+                <div className="form-container">
+                     <form onSubmit={submitBookReturn}>
+
+                        <p>Want to return this book?</p>
+                        <label htmlFor="email">{claimedByName}'s Email </label>
+                        <input onChange={emailChange} type="email" id="email" /> <br/>
+    
+                        <input type="submit" value="Return" id="submit"/>
+                    </form>
+                </div>
+            )
+        }
+
+    return (
+            <>
+             <div>{ claimedByName !== null ? ReturnForm() : ''}</div>   
+        
+             <p className="claim-book-error">{errorMessage}</p>
+            </>
+            )
+}
+
+export default ReturnBook
