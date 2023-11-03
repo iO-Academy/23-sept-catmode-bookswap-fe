9fc1a5ffc6d31d256cf28805d4f6b3f48ef848da
Timothy Williamson <atimiw@gmail.com>
2023-11-03 11:13:50 +0000

    Fixed issues as requested in the PR review.

--- src/Components/BookDetail/Reviews/ReviewSummary/ReviewSummary.jsx
+++ src/Components/BookDetail/Reviews/ReviewSummary/ReviewSummary.jsx
@@ -1,23 +1,19 @@
 function ReviewSummary ({reviews}) {
 
-    let reviewCount = {reviews}.reviews.length
-    let reviewArray = {reviews}.reviews
+    let reviewCount = reviews.length
     let reviewTally = 0
     let reviewAverage = 0
 
-    reviewArray.forEach(review => {
+    reviews.forEach(review => {
         reviewTally += review.rating
     })
 
     // Handle 0 review scenario, to fix NaN dividing by 0
-    if(reviewCount <= 0)
-        {
+    if(reviewCount <= 0){
         reviewAverage = 0    
-        }
-    else
-        {
+    } else {
         reviewAverage = Math.round((reviewTally / reviewCount) * 10) / 10            
-        }
+    }
 
     return (
         <p><a className="link" href="#reviews">{reviewCount} reviews</a> - {reviewAverage}/5 stars</p>
