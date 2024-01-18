ecfe16f25f8ae41f4d050391b17649e246f91beb
GDTimi <atimiw@gmail.com>
2024-01-18 14:45:49 +0000

    Filtering form in place.

--- src/Components/HomePage/Filter/Filter.jsx
+++ src/Components/HomePage/Filter/Filter.jsx
@@ -3,17 +3,34 @@ import "./Filter.css"
 function Filter() {
 
     return (
-        <div className="filter">
-        <label for="genre_id">Genre</label>
-        <select name="genre_id">
-          <option value="">All Genres</option>
-          <option value="1">Thriller</option>
-          <option value="2">Romance</option>
-          <option value="3">Historical</option>
-          <option value="4">Non-fiction</option>
-        </select>
+        <form>
+            <div className="filter">
+                <div>
+                    <label for="genre">Genre</label>
+                    <select name="genre">
+                        <option value="">All Genres</option>
+                        <option value="1">Thriller</option>
+                        <option value="2">Romance</option>
+                        <option value="3">Historical</option>
+                        <option value="4">Non-fiction</option>
+                    </select>
+                </div>        
+            
+                <div>
+                    <label for="search">Search</label>
+                    <input
+                    name="search"
+                    type="text"
+                    />
+                </div>  
 
-        </div>
+                <div>
+                    <button type="submit">
+                    Filter
+                    </button>
+                </div>                     
+            </div>
+        </form>    
     )
 
 }
